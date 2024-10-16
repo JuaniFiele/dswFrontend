808724701ac64be403703ecfc4e4c9c025459854
SrNanu <107449931+SrNanu@users.noreply.github.com>
2024-10-16 16:54:30 -0300

    Interface Created

---
+++ src/app/interfaces/attention.ts
@@ -0,0 +1,31 @@
+//import { ConsultationHours } from './consultation-hours';
+import { Patient } from './patient';
+
+export interface Attention {
+    id?: number;
+    date: Date;
+    
+    paymentDate: Date
+
+    result: string
+
+    reason: string
+
+    currentIllness: string
+
+    vitalSigns: string
+
+    physicalExamination: string;
+
+    diagnosis: string;
+
+    treatment: string;
+
+    observation: string;
+
+    dateCancelled: Date;
+
+    //consultationHours?: ConsultationHours
+    
+    patient?: Patient
+}
